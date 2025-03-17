63e85ce59de0ae190dd770d3209f6e44ee29efc7
Thom <thomvanrijn@gmail.com>
2025-03-17 13:26:22 +0700

    implemented onload function

--- js-script.js
+++ js-script.js
@@ -1,38 +1,56 @@
 
 const gridContainer = document.getElementById("grid-container");
+const rangeDisplay = document.getElementById("range-display");
+const rangeElement = document.getElementById("range-element");
+const body = document.getElementById("body")
+const btn = document.getElementById("btn");
+rangeDisplay.textContent = "16x16"
 
+body.onload = addGrid;
+btn.addEventListener("click", addGrid)
+rangeElement.addEventListener("input", rangeUpdate)
 
-    
-    
+function rangeUpdate(e) {
+    rangeDisplay.textContent = (e.target.value + "x" + e.target.value)
+}
 
+function debug() {
+    console.log("debug")
+}
 
+function addGrid() {
+    num = rangeElement.value
+    removeGrid()
 
-function addGrid(num){
-    
     for (let i = 0; i < num * num; i++) {
         const gridElement = document.createElement("div");
-        gridElement.className ="grid-element";
-        gridElement.style.height = ((100 / num)+"%")    
-        gridElement.style.width =  ((100 / num)+"%")  
+        gridElement.className = "grid-element";
+        gridElement.style.height = ((100 / num) + "%")
+        gridElement.style.width = ((100 / num) + "%")
         gridContainer.appendChild(gridElement)
     }
 
-    
 }
-addGrid(10)
+
+function removeGrid() {
+
+    gridContainer.innerHTML = ""
+}
+
+
 
 gridContainer.addEventListener("mouseover", etch)
-    
+
 
 function etch(e) {
-    if(e.target !== gridContainer) {
+    if (e.target !== gridContainer) {
         let currentElement = e.target;
         currentElement.style.border = "1px solid green"
     }
 }
 
 
-    
+
 
 
 
